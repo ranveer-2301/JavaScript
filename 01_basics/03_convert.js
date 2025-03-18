c79ba9ba428108580a6e32d2812f25ff4fc635ab
Ranveer Kumar Poddar <144588990+ranveer-2301@users.noreply.github.com>
2025-03-18 03:58:48 +0000

    datatype and conversion done

---
+++ 01_basics/03_convert.js
@@ -0,0 +1,22 @@
+number = "33"
+
+console.log(typeof(number));
+
+let valueInNumber = Number(number);
+
+console.log(typeof(valueInNumber));
+
+// "33" => 33
+// "3ad" => NaN
+//  true => 1
+// undefined => NaN
+// null => 0
+
+let isLooggedIn = 1
+let booleanIsLoggedIn = Boolean(isLooggedIn)
+
+console.log((booleanIsLoggedIn));
+
+
+// "" =>false
+// "ranveer" => true
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as newer version
+
+// alert() // we are using node js not browser
+
+// number 
+// bigint
+// string ""
+// boolean
+// null => standalone value (null is an object)
+// undefined =>
+// symbol => unique
+
+// object
+// typeof
