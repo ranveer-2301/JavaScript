2778b17ccb01ab8518a366cd336f162294df9b67
Ranveer Kumar Poddar <144588990+ranveer-2301@users.noreply.github.com>
2025-03-18 04:54:20 +0000

    comparision and datatype summary done

---
+++ 01_basics/04_omparision.js
@@ -0,0 +1,16 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// the eqaulity operator and the comparision operator both work differently
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+// === strict check (check's the datatype also)
+
