3507a92dea062fc139a506a9bf4264e6970cfc0d
Ranveer Kumar Poddar <144588990+ranveer-2301@users.noreply.github.com>
2025-03-18 06:43:47 +0000

    string num and maths done with changes in name of some file.

---
+++ 01_basics/06_string.js
@@ -0,0 +1,18 @@
+const name = "Ranveer"
+const repoCount = 50
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Veer')
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt('2'));
+console.log(gameName.indexOf('e'));
+
+// more mothods like slice => accessing from the back 
+// trim() => removes spaces
+
+// replace ,replaceAll etc 
+const url = "http://veer.com/veer%20kumar%20poddar"
+
+console.log(url.replaceAll('%20', '-'));
