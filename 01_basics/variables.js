e8f90a578c67724cb1cd558358d4b20cf9ee364d
Ranveer Kumar Poddar <144588990+ranveer-2301@users.noreply.github.com>
2025-03-18 03:40:43 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553 // can not be changed
+let accountEmail = "ranveer@gmail.com"
+var accountPassword = "123"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+console.log(accountId);
+accountEmail = "veer@gmail.com"
+accountPassword = "154621"
+accountCity = "Bengaluru"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword, accountCity, accountState]);
